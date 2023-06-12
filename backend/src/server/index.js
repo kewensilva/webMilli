786fec4bab0600f990ffde26d350041efa95812f
kewensilva <kewen.macedo@gmail.com>
2023-06-12 10:34:16 -0300

    adjustment file routes and index server

--- backend/src/server/index.js
+++ backend/src/server/index.js
@@ -3,13 +3,21 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const CategoryRoutes = require("../routes/routes.js");
+const db = require("../../models/index");
+const router = require('../routes/routes');
 
 
+db.sequelize.sync()
+    .then(() => {
+        console.log("Database Sincronizado!");
+    })
+    .catch((err) => {
+        console.log("Falha ao tentar Sincronizar Database: " + err.message);
+    });
 
 app.use(cors());
 app.use(express.json());
-app.use(CategoryRoutes);
+app.use(router);
 
 app.listen(3333);
 console.log("Server Iniciado: http://localhost:3333");
