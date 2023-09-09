0b9889106546de745043a89dea8a09694b9d74b9
kewensilva <kewen.macedo@gmail.com>
2023-09-09 14:03:10 -0300

    feat: commit context in project

--- front/src/components/Content/content.tsx
+++ front/src/components/Content/content.tsx
@@ -1,4 +1,4 @@
-import { FilterBar } from "../Filter/filter-bar"
+// import { FilterBar } from "../Filter/filter-bar"
 import { ProductsList } from "../products/products-list"
 export default function Content() {
   return (
